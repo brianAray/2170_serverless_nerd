917dc7cb0b5d399a224d98d4661732d41a9c2905
Brian Arayathel <brian.arayathel@revature.com>
2025-09-03 10:08:51 -0400

    node and activity

---
+++ examples/week_1/node_fundamentals/index.js
@@ -0,0 +1,8 @@
+// const { add } = require("./math_functions"); // CommonJS
+// import {add} from "./math_functions.js"; // ESModule
+
+const {logger} = require('./logger');
+
+// console.log(add(4, 2));
+
+logger.info("Hello");
---
+++ examples/week_1/node_fundamentals/math_functions.js
@@ -0,0 +1,13 @@
+// const add = (x, y) => {
+//     return x + y;
+// }
+
+export function add(x, y) {
+  return x + y;
+}
+
+// module.exports = {
+//   add,
+// };
+
+
